a754ae825135ecf368c413042be34a45093f4761
Eweer <oriol253@gmail.com>
2026-05-14 13:45:07 +0200

    Fix: Boss ball display in dungeon info

--- src/scripts/pokeballs/Pokeballs.ts
+++ src/scripts/pokeballs/Pokeballs.ts
@@ -170,7 +170,7 @@ class Pokeballs implements Feature {
             pokerus: App.game.party.getPokemon(id)?.pokerus,
             pokemonType: [pokemon.type1, pokemon.type2],
             encounterType,
-            category: App.game.party.getPokemon(id)?.category,
+            category: App.game.party.getPokemon(id)?.category ?? [],
         })?.ball() ?? GameConstants.Pokeball.None;
     }
 
